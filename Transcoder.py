6a30bff7dfb29a5d03feca6ffebeb54ec9afb6e1
jramseygreen <44009946+jramseygreen@users.noreply.github.com>
2022-02-25 13:06:52 +0000

    Update Transcoder.py

--- Transcoder.py
+++ Transcoder.py
@@ -1,6 +1,7 @@
 import os
 import time
 import magic
+import threading
 
 from Watcher import Watcher
 
@@ -21,6 +22,8 @@ class Transcoder:
         self.output_dir = output_dir
 
         self.watcher = Watcher(path=self.input_dir, on_created=self.on_created)
+        self.queue = []
+        self.running = False
 
     def start(self):
         try:
@@ -29,21 +32,31 @@ class Transcoder:
             os.mkdir(self.output_dir)
 
         finally:
+            self.running = True
+            threading.Thread(target=self.processor).start()
             self.watcher.start()
 
     def stop(self):
         self.watcher.stop()
+        self.running = False
+
+    def processor(self):
+        while self.running:
+            if self.queue:
+                event = self.queue.pop(0)
+                if not event.is_directory and os.path.exists(event.src_path):
+                    historicalSize = -1
+                    while (historicalSize != os.path.getsize(event.src_path)):
+                        historicalSize = os.path.getsize(event.src_path)
+                        time.sleep(1)
+                    time.sleep(1)
+                    if not os.path.exists(self.output_dir + os.sep + event.src_path.replace(self.input_dir + os.sep, "") + ".mp4") and self.mime.from_file(event.src_path).find("video") != -1:
+                        os.makedirs(os.path.dirname(self.processing_dir + os.sep + event.src_path.replace(self.input_dir + os.sep, "") + '.mp4'), exist_ok=True)
+                        os.system(self.ffmpeg_path + ' -i "' + event.src_path + '" -bsf:v h264_mp4toannexb -sn -map 0:0 -map 0:1 -vcodec libx264 "' + self.processing_dir + os.sep + event.src_path.replace(self.input_dir + os.sep, "") + '.mp4"')
+                        os.makedirs(os.path.dirname(self.output_dir + os.sep + event.src_path.replace(self.input_dir + os.sep, "") + '.mp4'), exist_ok=True)
+                        os.replace(self.processing_dir + os.sep + event.src_path.replace(self.input_dir + os.sep, "") + '.mp4', self.output_dir + os.sep + event.src_path.replace(self.input_dir + os.sep, "") + '.mp4')
+                    os.remove(event.src_path)
+            time.sleep(1)
 
     def on_created(self, event):
-        if not event.is_directory and os.path.exists(event.src_path):
-            historicalSize = -1
-            while (historicalSize != os.path.getsize(event.src_path)):
-                historicalSize = os.path.getsize(event.src_path)
-                time.sleep(1)
-            time.sleep(1)
-            if not os.path.exists(self.output_dir + os.sep + event.src_path.replace(self.input_dir + os.sep, "") + ".mp4") and self.mime.from_file(event.src_path).find("video") != -1:
-                os.makedirs(os.path.dirname(self.processing_dir + os.sep + event.src_path.replace(self.input_dir + os.sep, "") + '.mp4'), exist_ok=True)
-                os.system(self.ffmpeg_path + ' -i "' + event.src_path + '" -bsf:v h264_mp4toannexb -sn -map 0:0 -map 0:1 -vcodec libx264 "' + self.processing_dir + os.sep + event.src_path.replace(self.input_dir + os.sep, "") + '.mp4"')
-                os.makedirs(os.path.dirname(self.output_dir + os.sep + event.src_path.replace(self.input_dir + os.sep, "") + '.mp4'), exist_ok=True)
-                os.replace(self.processing_dir + os.sep + event.src_path.replace(self.input_dir + os.sep, "") + '.mp4', self.output_dir + os.sep + event.src_path.replace(self.input_dir + os.sep, "") + '.mp4')
-            os.remove(event.src_path)
+        self.queue.append(event)
